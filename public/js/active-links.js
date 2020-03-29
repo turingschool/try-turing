e274b0d5cee4868c28490e91f45c8a47176c9876
Amy Holt <amy.elizabeth.holt@gmail.com>
2020-03-29 10:49:14 -0600

    Add ability to show and hide Spicy Challenges

--- public/js/active-links.js
+++ public/js/active-links.js
@@ -1,3 +1,4 @@
+//active links in nav bar
 $('.nav-links a:not(.home-logo-link)').each(function(idx, link) {
   var currentPath = window.location.pathname;
   var linkPath = $(link).attr('href');
@@ -5,4 +6,20 @@ $('.nav-links a:not(.home-logo-link)').each(function(idx, link) {
   if (currentPath.includes(linkPath)) {
     $(link).toggleClass('active-link');
   }
+});
+
+//spicy challenge show/hide
+var allSpicyTitles = document.querySelectorAll(".spicy-click");
+
+allSpicyTitles.forEach(function(title) {
+
+  title.addEventListener("click", function(e) {
+    var content = e.target.nextSibling.nextSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+
 });
